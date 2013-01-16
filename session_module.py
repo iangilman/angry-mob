a394d4e7439c7aaedaecfd899531a793e5e0cfc8
Ian Gilman <ian@iangilman.com>
2013-01-16 14:59:58 -0800

    Fixed whitespace

--- session_module.py
+++ session_module.py
@@ -9,24 +9,24 @@ from webapp2_extras import sessions
 #Runs first in the whole application
 myconfig_dict = {}
 myconfig_dict['webapp2_extras.sessions'] = {
-    'secret_key': 'aegoradhfgnfiosgbnodfngs',
+  'secret_key': 'aegoradhfgnfiosgbnodfngs',
 }
 
 #Session Handling class, gets the store, dispatches the request
 class BaseSessionHandler(webapp2.RequestHandler):
-    def dispatch(self):
-        # Get a session store for this request.
-        self.session_store = sessions.get_store(request=self.request)
+  def dispatch(self):
+    # Get a session store for this request.
+    self.session_store = sessions.get_store(request=self.request)
 
-        try:
-            # Dispatch the request.
-            webapp2.RequestHandler.dispatch(self)
-        finally:
-            # Save all sessions.
-            self.session_store.save_sessions(self.response)
+    try:
+      # Dispatch the request.
+      webapp2.RequestHandler.dispatch(self)
+    finally:
+      # Save all sessions.
+      self.session_store.save_sessions(self.response)
 
-    @webapp2.cached_property
-    def session(self):
-        # Returns a session using the default cookie key.
-        return self.session_store.get_session()
+  @webapp2.cached_property
+  def session(self):
+    # Returns a session using the default cookie key.
+    return self.session_store.get_session()
 #End of BaseSessionHandler Class
